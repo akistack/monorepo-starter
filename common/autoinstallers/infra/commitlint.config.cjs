8215fac82c808d92aec0b0d137ac7e8e06f1692a
Ayase Aki <kirainmoe@gmail.com>
2026-01-31 00:53:14 +0800

    feat(infra): add commitlint & lint-staged

--- common/autoinstallers/infra/commitlint.config.cjs
+++ common/autoinstallers/infra/commitlint.config.cjs
@@ -3,12 +3,5 @@ module.exports = {
   rules: {
     'header-max-length': [2, 'always', 150],
     'subject-full-stop': [0, 'never'],
-    'subject-case': [
-      2,
-      'never',
-      [
-        'upper-case', // UPPERCASE
-      ],
-    ],
   },
 };
---
+++ common/autoinstallers/infra/lint-staged.config.cjs
@@ -0,0 +1,3 @@
+module.exports = {
+  '**/*.{ts,tsx,js,jsx,mjs,cjs,mts,cts}': 'biome check --write',
+};
